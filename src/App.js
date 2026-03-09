87f41a5c1d7312b557d38e2c158012a2d89e48a4
NAEEM MUNTASIR ASIF <138962295+muntasir010@users.noreply.github.com>
2026-03-09 20:50:17 +0600

    feat: Add ReactFlowProvider to App component
    Wrap components in ReactFlowProvider for context.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { PipelineToolbar } from './toolbar';
-import { PipelineUI } from './ui';
-import { SubmitButton } from './submit';
+import { ReactFlowProvider } from "reactflow";
+import { PipelineToolbar } from "./toolbar";
+import { PipelineUI } from "./ui";
+import { SubmitButton } from "./submit";
 
 function App() {
   return (
-    <div>
-      <PipelineToolbar />
-      <PipelineUI />
-      <SubmitButton />
+    <div style={{ height: "100vh", width: "100vw" }}>
+      {/* Pipeline Toolbar */}
+      <ReactFlowProvider>
+        <PipelineToolbar />
+
+        {/* Pipeline UI */}
+        <PipelineUI />
+
+        {/* Submit Button */}
+        <SubmitButton />
+      </ReactFlowProvider>
     </div>
   );
 }
